9c0b6e11cacd6c8ed34bde5f1669057f777a10bc
Adam Shell <adam@granseal.net>
2023-12-01 12:28:31 -0500

    Day 1

---
+++ src/day1/mod.rs
@@ -0,0 +1,83 @@
+use std::collections::HashMap;
+
+pub fn day1_tests() {
+    part1(include_str!("test.txt"));
+    part2(include_str!("test2.txt"));
+}
+pub fn day1() {
+    part1(include_str!("input.txt"));
+    part2(include_str!("input.txt"));
+}
+
+pub fn part1(input: &str) {
+    let result = input.lines().map(|l| {
+        let mut result1 = None;
+        while result1.is_none() {
+            for i in 0..l.len() {
+                if l[i..i+1].parse::<i32>().is_ok() {
+                    result1 = Some(l[i..i+1].to_owned());
+                    break;
+                }
+            }
+        }
+        let mut result2 = None;
+        while result2.is_none() {
+            for i in (0..l.len()).rev() {
+                if l[i..i+1].parse::<i32>().is_ok() {
+                    result2 = Some(l[i..i+1].to_owned());
+                    break;
+                }
+            }
+        }
+        let result3 = format!("{}{}",result1.unwrap(),result2.unwrap()).parse::<i32>().unwrap();
+        result3
+    }).collect::<Vec<_>>();
+    //println!("{:?}",result);
+    println!("{:?}",result.iter().sum::<i32>());
+}
+
+
+
+pub fn part2(input: &str) {
+    let mut digits = HashMap::from(
+        [("one","1"),("two","2"),("three","3"),("four","4"),("five","5"),
+            ("six","6"),("seven","7"),("eight","8"),("nine","9")]
+    );
+
+    let result = input.lines().map(|l| {
+        let mut result1 = None;
+        'first: while result1.is_none() {
+            for i in 0..l.len() {
+                for (k,v) in digits.iter() {
+                    if l[i..].starts_with(*k) {
+                        result1 = Some(v.to_string());
+                        break 'first;
+                    }
+                }
+                if l[i..i+1].parse::<i32>().is_ok() {
+                    result1 = Some(l[i..i+1].to_owned());
+                    break;
+                }
+            }
+        }
+        let mut result2 = None;
+        'second: while result2.is_none() {
+            for i in (0..l.len()).rev() {
+                for (k,v) in digits.iter() {
+                    if l[i..].starts_with(*k) {
+                        result2 = Some(v.to_string());
+                        break 'second;
+                    }
+                }
+                if l[i..i+1].parse::<i32>().is_ok() {
+                    result2 = Some(l[i..i+1].to_owned());
+                    break;
+                }
+            }
+        }
+        let result3 = format!("{}{}",result1.unwrap(),result2.unwrap()).parse::<i32>().unwrap();
+        result3
+    }).collect::<Vec<_>>();
+    println!("{:?}",result.iter().sum::<i32>());
+}
+
